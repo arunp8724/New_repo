31123e0540dec06a9dfa3c8d34e18083c44e8da3
arunp8724 <ashishchavan9867@gmail.com>
2025-03-13 00:17:44 +0530

    Revert "Delete"
    This reverts commit a14dddf6b6dc3c60ee5137d86e6f066790a73b46.

--- Basic.py
+++ Basic.py
@@ -4,6 +4,4 @@ num1 = 45
 num2 = 67
 print(num1 - num2) # To sub 2 numbers
 print(num1 + num2) # To add 2 numbers
-print(num1 * num2) # To mult 2 numbers 
-
-print("Thir unnessary commit")
+print(num1 * num2) # To mult 2 numbers
